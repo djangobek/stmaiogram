561add2d7ef430bab0619ea39ca00979ea26555f
djangobek <nnjjmmjj8@gmail.com>
2025-04-03 16:36:53 +0500

    aa

--- handlers/users/test.py
+++ handlers/users/test.py
@@ -18,42 +18,16 @@ async def admin_click(message: types.Message):
 
 @dp.message(lambda message: message.text == "📘 Qo'llanma")
 async def sendqullanma(message: types.Message):
-    caption_text = (
-        "📚 *Test Ishlash va Javoblarni Tekshirish Boti* 🤖\n\n"
-        "Assalomu alaykum! 🎯\n"
-        "Yangi bot orqali test ishlash va javoblaringizni tekshirish imkoniyati! 🚀\n\n"
-        "✅ *Botning imkoniyatlari:*\n"
-        "- Test savollarini yechish ✍️\n"
-        "- Javoblaringizni avtomatik tekshirish ✅\n"
-        "- To'g'ri va noto'g'ri javoblaringizni ko'rsatish 📊\n\n"
-        "📌 *Qanday ishlatish kerak?*\n"
-        "1. Botga /start buyrug'ini yuboring.\n"
-        "2. Test savollarini oling va javoblaringizni yozing.\n"
-        "3. Javoblaringizni botga yuboring va natijalaringizni oling.\n\n"
-        "📩 *Javoblarni tekshirish:*  \n"
-        "Bot sizning javoblaringizni avtomatik tekshiradi, to'g'ri va noto'g'ri javoblaringizni ko'rsatadi. 📝\n\n"
-        "📊 *Natijalaringizni bilish:*  \n"
-        "Har bir test yakunida to'liq statistikangizni olishingiz mumkin. 📈\n\n"
-        "👉 *Boshlash uchun:* [/start](/start)\n\n"
-        "ℹ️ *Qo'shimcha ma'lumot:*  \n"
-        "Agar savollaringiz bo'lsa, botga yozing yoki biz bilan bog'laning. \n"
-        "@zohidxon_rasaxonov"
-    )
+    
     await message.answer_video(
         video="BAACAgIAAxkBAAI94WfuTglxIh24tMrwoPw1OfMjR46AAAIHYwACooN4S7LhFx4K8UK-NgQ",  # Sending using the video ID
-        caption=caption_text,
+        caption="Bu yerda Qanday qilib TEST Yaratish  haqida ma'lumotlar mavjud.\n\n",
         
         parse_mode="HTML"  # Optional: Use Markdown or HTML for formatting
     )
     await message.answer_video(
         video="BAACAgIAAxkBAAI94GfuTgliN39JjMvJzTEDfcjyx54dAAIGYwACooN4S7iMywgcchnONgQ",  # Sending using the video ID
         caption="Bu yerda Qanday qilib TEST ISHLASH  haqida ma'lumotlar mavjud.\n\n"
-        "❕ Test ishlash uchun videoni ko`ring:\n",
-        parse_mode="HTML"  # Optional: Use Markdown or HTML for formatting
-    )
-    await message.answer_video(
-        video="BAACAgIAAxkBAAI932fuTglSEMSvFwzVzc-XG8LHyaF7AAICYwACooN4S4mF8Cy9LESJNgQ",  # Sending using the video ID
-        caption="Endi Siz botdan bemalol foydalana olasiz !.\n\n"
         "❕ Test ishlash uchun videoni ko`ring:\n",
         parse_mode="HTML"  # Optional: Use Markdown or HTML for formatting
     )
